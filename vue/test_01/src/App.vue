fbf393e733f0f7939a12ed06b35297dbfb7a0297
Administrator <lijtforjava@sina.com>
2019-04-04 14:21:51 +0800

    vue

---
+++ vue/test_01/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <SlotDemo>父级插槽内容 <strong>{{ scope }}</strong></SlotDemo>
+    <SlotDemo><template v-slot:header><h4>具名插槽原有写法</h4></template></SlotDemo>
+    <SlotDemo><header>具命插槽的新的写法</header></SlotDemo>
+    <SlotDemo><footer scope="sunObj"><h3 style="color:red;"> {{sunObj.msg}} </h3></footer></SlotDemo>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import SlotDemo from './components/SlotDemo'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SlotDemo
+  },
+  data() {
+    return {
+      scope: '编译作用域'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ vue/test_01/src/components/SlotDemo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 v-once>{{ title }}</h1>
+    <slot>插槽默认内容</slot>
+    <slot name="header"></slot>
+    <slot name="footer" :msg="message"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlotDemo',
+  data () {
+    return {
+      title: '插槽',
+      message: '子组件的内容'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
